b232b83c2b0d9435b81bc10f208926f1870baa75
Anne <anne.k.plummer@gmail.com>
2020-08-16 20:02:23 -0500

    added html and have engineer branch of question started

--- index.js
+++ index.js
@@ -2,16 +2,111 @@ const inquirer = require ('inquirer');
 //const generateHTML = require('./dist/team.html')
 //const { writeFile } = require('./src/generateSite.js')
 
-//array of questions for user
+
+//const teamApp = () => {}
+    const newManager =() => {
+        return inquirer.prompt([
+            {
+                type: "input",
+                name: "managerName",
+                message: "What is the manager's name?",
+                validate: managerNameInput => {
+                    if (managerNameInput){
+                        return true;
+                    } else {
+                        console.log('Please enter a name.')
+                    }
+                }
+            },
+            {
+                type: "input",
+                name: "managerId",
+                message: "What is the manager's employee ID?",
+                validate: managerIdInput => {
+                    if (isNaN(managerIdInput)){
+                        console.log("Please enter a valid number");
+                    } else {
+                        return true;
+                    }
+                }
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "What is the manager's email address?",
+                validate: function (managerEmailInput) {
+  
+                     valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(managerEmailInput)
+  
+                    if (valid) {
+                        return true;
+                    } else {
+                        console.log("Please enter a valid email.")
+                        return false;
+                    }
+                }
+            },
+            {
+                type: "input",
+                name: "officeNumber",
+                message: "What is the manager's office phone number?",
+                validate: officeNumberInput => {
+                    if (isNaN(officeNumberInput)){
+                        console.log("Please enter a valid number");
+                    } else {
+                        return true;
+                    }
+                }
+            },
+        ])
+    };
+        /*.then(answers => {
+            const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.OfficeNumber);
+            teamMembers.push(manager);
+            idArray.push(answers.managerId);
+            createTeam(); 
+          }); */
+
+    const createTeam = () => {
+        return inquirer.prompt([
+            {
+                type: 'checkbox',
+                name: 'role',
+                message: "What kind of employee would you like to add?",
+                choices: ['Engineer', 'Intern'],
+                validate: roleInput => {
+                    if (roleInput){
+                        return true;
+                    } else {
+                    console.log('Please select a role.');
+                    return false;
+                    }
+                }
+            },
+        ]).then(function (responses){
+                if (responses.role = 1){
+                    return inquirer.prompt([
+                        {
+                            type: "input",
+                            name: "nameEngineer",
+                            message:"What is the Engineer's name?",
+                            validate: nameEngineerInput => {
+                                if (nameEngineerInput){
+                                    return true;
+                                } else {
+                                    console.log("Please enter the Engineer's name")
+                                }
+                            }
+                        },
+                    ])
+                }
+            }
+        )
+    };
+
+/*//array of questions for user
 const newEmployee = () => {
     return inquirer.prompt([
-        
-        {
-            type: 'input',
-            name: 'officeNumber',
-            message: "What is the Manager's office phone number?",
-            when: (answers) => answers.roleInput === 'Manager',
-        },
         {
             type: 'input',
             name: 'employeeName',
@@ -39,35 +134,6 @@ const newEmployee = () => {
     ]);
 };
 
-const roleQuestion = () => {
-    return inquirer.prompt([
-        {
-            type: 'checkbox',
-            name: 'role',
-            message: "What is the Employee's role?",
-            choices: ['Manager', 'Engineer', 'Intern'],
-            validate: roleInput => {
-                if (roleInput){
-                    return true;
-                } else {
-                    console.log('Please select a role.');
-                    return false;
-                }
-            } 
-        }
-    ])
-};
-
-const manager = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'phoneNumber',
-            message: "What is the manager's phone number?",
-        },
-    ])
-};
-
 const engineer = () => {
     return inquirer.prompt([
         {
@@ -86,13 +152,19 @@ const intern = () => {
             message: "Where do you go to school?",
         },
     ])
-};
+}; */
 
-newEmployee()
+        
+
+/*newEmployee()
     .then(roleQuestion)
+        //when: (answers) => answers.roleInput === 'Manager',
     .then(intern)
     .then(engineer)
     .then(manager)
     .catch(err => {
         console.log(err);
-    });
+    }); */
+
+newManager()
+    .then(createTeam);
